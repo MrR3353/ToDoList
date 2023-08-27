fe372fea31ac8f1363cd265e2f3104dd55828f2f
AnnSamson <mishinstepan2000@gmail.com>
2023-08-27 18:19:11 +0500

    rest api for tasks

--- todo_drf/api/urls.py
+++ todo_drf/api/urls.py
@@ -4,4 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.api_overview, name='api-overview'),
     path('task-list/', views.task_list, name='task-list'),
+    path('task/<str:id>/', views.task, name='task'),
+    path('task-create/', views.task_create, name='task-create'),
+    path('task-update/<str:id>', views.task_update, name='task-update'),
+    path('task-delete/<str:id>', views.task_delete, name='task-delete'),
 ]
+
+
+
--- todo_drf/api/views.py
+++ todo_drf/api/views.py
@@ -13,10 +13,10 @@ from .serializers import TaskSerializer
 def api_overview(request):
     api_urls = {
         'List': '/task-list/',
-        'Detail View': '/task-detail/<str:pk>/',
+        'Detail View': '/task/<str:id>/',
         'Create': '/task-create/',
-        'Update': '/task-update/<str:pk>/',
-        'Delete': '/task-delete/<str:pk>/',
+        'Update': '/task-update/<str:id>/',
+        'Delete': '/task-delete/<str:id>/',
     }
     return Response(api_urls)
 
@@ -26,3 +26,34 @@ def task_list(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def task(request, id):
+    task = Task.objects.get(id=id)
+    serializer = TaskSerializer(task, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def task_create(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def task_update(request, id):
+    task = Task.objects.get(id=id)
+    serializer = TaskSerializer(instance=task, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def task_delete(request, id):
+    task = Task.objects.get(id=id)
+    task.delete()
+    return Response()
