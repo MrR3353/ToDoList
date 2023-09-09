07cbc3ed701b0f7f2a398601fa83ba491bbc7ae7
MrR3353 <mishinstepan2000@gmail.com>
2023-09-09 12:41:50 +0300

    Added frontend with react

--- todo_drf/todo_drf/settings.py
+++ todo_drf/todo_drf/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ij-gg2*1r%+m1nyv8r6m2$jo9fu@s-fce4wqkon7q3q8)-k98i
+SECRET_KEY = 'django-insecure-ij-gg2*1r%+m1nyv8r6m2$jo9fu@s-fce4wqkon7q3q8)-k98i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
